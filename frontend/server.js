b2cb121223b95e79d5edf750b40651d8162e967b
Sai Katari <saikatari99@gmail.com>
2026-04-11 14:44:35 +0530

    Fix frontend server

--- frontend/server.js
+++ frontend/server.js
@@ -5,12 +5,12 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(__dirname));
 
-app.get("/", (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log("Frontend running on port " + PORT);
+  console.log("Server running on port", PORT);
 });
